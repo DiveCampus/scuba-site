bf0a2c6276842d8e13bf886814d10e77a1b44c84
Kadir70-dev <kadirab1999@gmail.com>
2026-06-09 13:59:56 +0530

    Improve test setup and silence Vitest console noise

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -13,6 +13,8 @@ import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
 import { afterEach, vi } from "vitest";
 
+vi.spyOn(console, "log").mockImplementation(() => {});
+
 /* ---------------------------------------------------------------- polyfills */
 
 if (!window.matchMedia) {
